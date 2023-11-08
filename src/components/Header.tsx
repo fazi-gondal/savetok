ca3c4f801254937e4976960bfbef090b7bfa5024
fazi-gondal <146380071+fazi-gondal@users.noreply.github.com>
2023-11-08 19:12:42 +0500

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import {
+  useMantineColorScheme,
+  Drawer,
+  Group,
+  ActionIcon,
+  Button,
+  Anchor,
+  Title,
+  Text,
+  Space,
+  Divider,
+  List,
+  ThemeIcon,
+} from '@mantine/core';
+import { IconCheck, IconSun, IconMoonStars } from '@tabler/icons';
+import { headerData } from '../data';
+
+const Header: React.FC = () => {
+  const [opened, setOpened] = useState(false);
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
+  return (
+    <Group position='apart' mt='md'>
+      <Anchor
+        underline={false}
+        size={30}
+        variant='text'
+        weight='bold'
+        color='grape'
+      >
+        SaveTok
+      </Anchor>
+
+      <Drawer
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title='About'
+        padding='xl'
+        position='right'
+        size='xl'
+      >
+        <Divider my='sm' />
+        <Title order={2} color='grape'>
+          SaveTok by Fazi.
+        </Title>
+        <Text>{headerData.subTitle}</Text>
+        <Space h='lg' />
+        <Text>{headerData.description}</Text>
+        <Space h='lg' />
+
+        {headerData.faq?.map((data: any, idx: number) => (
+          <div key={idx}>
+            <Title order={5}>{data.question}</Title>
+            <Text>{data.answer}</Text>
+            <Space h='lg' />
+          </div>
+        ))}
+
+        <Title order={5}>Tech used</Title>
+        <List
+          spacing='xs'
+          size='sm'
+          mt='xs'
+          center
+          icon={
+            <ThemeIcon color='teal' size={16} radius='xl'>
+              <IconCheck size={14} />
+            </ThemeIcon>
+          }
+        >
+          {headerData.tech?.map((data: any, idx: number) => (
+            <List.Item key={idx + data}>{data}</List.Item>
+          ))}
+        </List>
+        <Space h='xl' />
+        <Title order={5}>Developed by</Title>
+        <Text>{headerData.dev}</Text>
+      </Drawer>
+      <Group>
+        <ActionIcon
+          variant='transparent'
+          color={dark ? 'yellow' : 'blue'}
+          onClick={() => toggleColorScheme()}
+          title={dark ? 'Light mode' : 'Dark mode'}
+        >
+          {dark ? '☀️' : '🌙'}
+        </ActionIcon>
+        <Button variant='light' color='grape' onClick={() => setOpened(true)}>
+          About
+        </Button>
+      </Group>
+    </Group>
+  );
+};
+
+export default Header;
---
+++ src/store/historyStore.ts
@@ -0,0 +1,38 @@
+import create from 'zustand';
+import { showNotification } from '@mantine/notifications';
+import { db } from '../db/dexie';
+
+interface History {
+  copyLink: (url: string) => void;
+  deleteVideo: (id: number) => void;
+  clearHistory: (historyLength: number | undefined) => void;
+  trimTitle: (str: string) => string;
+}
+
+export const useHistory = create<History>((set) => ({
+  copyLink: (url: string) => {
+    navigator.clipboard.writeText(url);
+    showNotification({
+      message: '🙂 Url copied.',
+      color: 'grape',
+    });
+  },
+  deleteVideo: (id: number) => {
+    db.history.delete(id);
+  },
+  clearHistory: (historyLength: number | undefined) => {
+    if (historyLength === 0) {
+      showNotification({
+        message: '⚠️ No history to be deleted.',
+        color: 'yellow',
+      });
+      return null;
+    }
+    db.history.clear();
+  },
+  trimTitle: (str: string) => {
+    if (str.length < 16) return str;
+
+    return str.substring(0, 15) + '..'.trim();
+  },
+}));
