a50f39d9c9a5383d2410cacb1a953d811a6ca485
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2025-03-07 06:58:39 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
             <Header />
 
             <Title order={3} align='center' sx={{ marginTop: '4em' }}>
-              Tiktok Video Downloader by fazi🧡
+              Tiktok Video Downloader by Nisar & Zain.
             </Title>
             <Title order={6} align='center'>
               Download Tiktok Videos Without Watermark
