f293c4d29928a0285178083da7b4a01664d5ab1b
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2025-03-07 06:58:43 -0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header: React.FC = () => {
       >
         <Divider my='sm' />
         <Title order={2} color='grape'>
-          SaveTok by Fazi.
+          SaveTok by Nisar & Zain.
         </Title>
         <Text>{headerData.subTitle}</Text>
         <Space h='lg' />
